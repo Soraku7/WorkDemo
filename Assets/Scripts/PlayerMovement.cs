fd8c89cc2d7cebda21315af6a15e045ab1e44841
张宇欣 <2673140287@qq.com>
2025-11-09 23:42:15 +0800

    完成自动寻路

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private Collider2D coll;
+    private Animator anim;
+    private NavMeshAgent agent;
+
+    public float speed;
+    private Vector2 movement;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        coll = GetComponent<Collider2D>();
+        anim = GetComponent<Animator>();
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Start()
+    {
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
+    }
+
+    private void Update()
+    {
+        Movement();
+        SwitchAnim();
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector2 target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            agent.SetDestination(target);
+        }
+    }
+
+    void Movement()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+        rb.MovePosition(rb.position + movement * speed * Time.deltaTime);
+
+    }
+
+    void SwitchAnim()
+    {
+        if (movement != Vector2.zero)
+        {
+            anim.SetFloat("horizontal", movement.x);
+            anim.SetFloat("vertical", movement.y);
+        }
+        anim.SetFloat("speed", movement.magnitude);
+    }
+}
